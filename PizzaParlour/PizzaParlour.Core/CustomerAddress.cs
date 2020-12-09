e174304a97e8e0ad9e445599d4e1452246307231
willvelida <willvelida@hotmail.co.uk>
2020-12-09 19:28:54 +1300

    Adding PizzaParlour Solution. Adding Customer and CustomerAddress classes

---
+++ PizzaParlour/PizzaParlour.Core/CustomerAddress.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaParlour.Core
+{
+    public class CustomerAddress
+    {
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
