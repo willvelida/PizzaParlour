79f71cc4ab5afd40122a4fab8d3e995191feca31
willvelida <willvelida@hotmail.co.uk>
2020-12-11 19:58:11 +1300

    Adding UpdateCustomer function. Adding Unit tests for UpdateCustomer function

---
+++ PizzaParlour/PizzaParlour.CustomerManager.API.UnitTests/FunctionTests/UpdateCustomerShould.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using PizzaParlour.Core.Models;
+using PizzaParlour.CustomerManager.API.Functions;
+using PizzaParlour.CustomerManager.API.Repositories;
+using PizzaParlour.CustomerManager.API.UnitTests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PizzaParlour.CustomerManager.API.UnitTests.FunctionTests
+{
+    public class UpdateCustomerShould
+    {
+        private Mock<ILogger<UpdateCustomer>> _loggerMock;
+        private Mock<ICustomerRepository> _customerRepoMock;
+        private Mock<ICustomerAggregateRepository> _customerAggregateRepoMock;
+        private Mock<HttpRequest> _httpRequestMock;
+
+        private UpdateCustomer _func;
+
+        public UpdateCustomerShould()
+        {
+            _loggerMock = new Mock<ILogger<UpdateCustomer>>();
+            _customerRepoMock = new Mock<ICustomerRepository>();
+            _customerAggregateRepoMock = new Mock<ICustomerAggregateRepository>();
+            _httpRequestMock = new Mock<HttpRequest>();
+
+            _func = new UpdateCustomer(
+                _loggerMock.Object,
+                _customerAggregateRepoMock.Object,
+                _customerRepoMock.Object);
+        }
+
+        [Fact]
+        public async Task UpdateCustomerSuccessfully()
+        {
+            // Arrange
+            var customer = TestDataGenerator.GenerateCustomer();
+            byte[] byteArray = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(customer));
+            MemoryStream memoryStream = new MemoryStream(byteArray);
+            _httpRequestMock.Setup(r => r.Body).Returns(memoryStream);
+
+            _customerAggregateRepoMock.Setup(c => c.GetCustomerById(It.IsAny<string>())).ReturnsAsync(customer);
+            _customerRepoMock.Setup(c => c.AddCustomer(It.IsAny<Customer>())).Returns(Task.CompletedTask);
+
+            // Act
+            var response = await _func.Run(_httpRequestMock.Object, customer.CustomerId);
+
+            // Assert
+            Assert.Equal(typeof(StatusCodeResult), response.GetType());
+            var responseAsStatusCode = (StatusCodeResult)response;
+            Assert.Equal(200, responseAsStatusCode.StatusCode);
+        }
+
+        [Fact]
+        public async Task Throw400OnBadRequest()
+        {
+            // Arrange
+            var customer = new Customer();
+            byte[] byteArray = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(customer));
+            MemoryStream memoryStream = new MemoryStream(byteArray);
+            _httpRequestMock.Setup(r => r.Body).Returns(memoryStream);
+
+            // Act
+            var response = await _func.Run(_httpRequestMock.Object, null);
+
+            // Assert
+            Assert.Equal(typeof(StatusCodeResult), response.GetType());
+            var responseAsStatusCode = (StatusCodeResult)response;
+            Assert.Equal(400, responseAsStatusCode.StatusCode);
+        }
+
+        [Fact]
+        public async Task Throw404OnNotFound()
+        {
+            // Arrange
+            var customer = TestDataGenerator.GenerateCustomer();
+            byte[] byteArray = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(customer));
+            MemoryStream memoryStream = new MemoryStream(byteArray);
+            _httpRequestMock.Setup(r => r.Body).Returns(memoryStream);
+
+            double fakeRU = 0.0;
+
+            _customerAggregateRepoMock.Setup(s => s.GetCustomerById(It.IsAny<string>()))
+                .Throws(new CosmosException("Not found", System.Net.HttpStatusCode.NotFound, 404, "someActivity", fakeRU));
+
+            // Act
+            var response = await _func.Run(_httpRequestMock.Object, customer.CustomerId);
+
+            // Assert
+            Assert.Equal(typeof(StatusCodeResult), response.GetType());
+            var responseAsStatusCode = (StatusCodeResult)response;
+            Assert.Equal(404, responseAsStatusCode.StatusCode);
+        }
+
+        [Fact]
+        public async Task Throw500ForInternalServerError()
+        {
+            // Arrange
+            var customer = TestDataGenerator.GenerateCustomer();
+            byte[] byteArray = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(customer));
+            MemoryStream memoryStream = new MemoryStream(byteArray);
+            _httpRequestMock.Setup(r => r.Body).Returns(memoryStream);
+
+            _customerAggregateRepoMock.Setup(s => s.GetCustomerById(It.IsAny<string>())).Throws(new Exception("some error"));
+
+            // Act
+            var response = await _func.Run(_httpRequestMock.Object, customer.CustomerId);
+
+            // Assert
+            Assert.Equal(typeof(StatusCodeResult), response.GetType());
+            var responseAsStatusCode = (StatusCodeResult)response;
+            Assert.Equal(500, responseAsStatusCode.StatusCode);
+        }
+    }
+}
--- PizzaParlour/PizzaParlour.CustomerManager.API/Functions/UpdateCustomer.cs
+++ PizzaParlour/PizzaParlour.CustomerManager.API/Functions/UpdateCustomer.cs
@@ -1,31 +1,37 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using PizzaParlour.CustomerManager.API.Repositories;
+using PizzaParlour.Core.Models;
 using Microsoft.Azure.Cosmos;
 
 namespace PizzaParlour.CustomerManager.API.Functions
 {
-    public class DeleteCustomer
+    public class UpdateCustomer
     {
-        private readonly ILogger<DeleteCustomer> _logger;
+        private readonly ILogger<UpdateCustomer> _logger;
         private readonly ICustomerAggregateRepository _customerAggregateRepository;
+        private readonly ICustomerRepository _customerRepository;
 
-        public DeleteCustomer(
-            ILogger<DeleteCustomer> logger,
-            ICustomerAggregateRepository customerAggregateRepository)
+        public UpdateCustomer(
+            ILogger<UpdateCustomer> logger,
+            ICustomerAggregateRepository customerAggregateRepository,
+            ICustomerRepository customerRepository)
         {
             _logger = logger;
             _customerAggregateRepository = customerAggregateRepository;
+            _customerRepository = customerRepository;
         }
 
-        [FunctionName(nameof(DeleteCustomer))]
+        [FunctionName(nameof(UpdateCustomer))]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "Customer/{id}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "Customer/{id}")] HttpRequest req,
             string id)
         {
             IActionResult result = null;
@@ -34,12 +40,23 @@ namespace PizzaParlour.CustomerManager.API.Functions
             {
                 if (string.IsNullOrEmpty(id))
                 {
-                    _logger.LogError($"Customer Id is not supplied");
+                    _logger.LogError($"Customer Id has not been specified");
                     result = new StatusCodeResult(StatusCodes.Status400BadRequest);
                 }
+                else
+                {
+                    var oldCustomer = await _customerAggregateRepository.GetCustomerById(id);
 
-                await _customerAggregateRepository.DeleteCustomer(id);
-                result = new StatusCodeResult(StatusCodes.Status200OK);
+                    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+                    var newCustomer = JsonConvert.DeserializeObject<Customer>(requestBody);
+
+                    newCustomer.CustomerId = oldCustomer.CustomerId;
+
+                    await _customerRepository.AddCustomer(newCustomer);
+
+                    result = new StatusCodeResult(StatusCodes.Status200OK);
+                }                                
             }
             catch (CosmosException cex) when (cex.StatusCode == System.Net.HttpStatusCode.NotFound)
             {
