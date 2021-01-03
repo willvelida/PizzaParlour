5f3e85761f4798d298c25b15bf78e8f6234e83d6
willvelida <willvelida@hotmail.co.uk>
2021-01-03 20:39:11 +1300

    Adding passing tests for CreateOrder endpoint

--- PizzaParlour/PizzaParlour.OrderManager.API.UnitTests/FunctionTests/CreateOrderShould.cs
+++ PizzaParlour/PizzaParlour.OrderManager.API.UnitTests/FunctionTests/CreateOrderShould.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Newtonsoft.Json;
+using PizzaParlour.Core.Models;
 using PizzaParlour.OrderManager.API.Functions;
 using PizzaParlour.OrderManager.API.Repositories;
+using PizzaParlour.OrderManager.API.UnitTests.Helpers;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace PizzaParlour.OrderManager.API.UnitTests.FunctionTests
 {
@@ -30,6 +37,46 @@ namespace PizzaParlour.OrderManager.API.UnitTests.FunctionTests
                 _loggerMock.Object,
                 _configMock.Object,
                 _orderRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateOrderSuccessfully()
+        {
+            // Arrange
+            var order = TestDataGenerator.GenerateOrder();
+            byte[] byteArray = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(order));
+            MemoryStream memoryStream = new MemoryStream(byteArray);
+            _httpRequestMock.Setup(r => r.Body).Returns(memoryStream);
+
+            _orderRepositoryMock.Setup(s => s.AddOrder(It.IsAny<Order>())).Returns(Task.CompletedTask);
+
+            // Act
+            var response = await _func.Run(_httpRequestMock.Object);
+
+            // Assert
+            Assert.Equal(typeof(StatusCodeResult), response.GetType());
+            var responseAsStatusCode = (StatusCodeResult)response;
+            Assert.Equal(201, responseAsStatusCode.StatusCode);
+        }
+
+        [Fact]
+        public async Task Throw500OnInternalServerError()
+        {
+            // Arrange
+            var order = TestDataGenerator.GenerateOrder();
+            byte[] byteArray = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(order));
+            MemoryStream memoryStream = new MemoryStream(byteArray);
+            _httpRequestMock.Setup(r => r.Body).Returns(memoryStream);
+
+            _orderRepositoryMock.Setup(s => s.AddOrder(It.IsAny<Order>())).Throws(new Exception("Some error"));
+
+            // Act
+            var response = await _func.Run(_httpRequestMock.Object);
+
+            // Assert
+            Assert.Equal(typeof(StatusCodeResult), response.GetType());
+            var responseAsStatusCode = (StatusCodeResult)response;
+            Assert.Equal(500, responseAsStatusCode.StatusCode);
         }
     }
 }
